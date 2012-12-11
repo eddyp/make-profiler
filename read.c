4c5fa17198eb64a959c113e47e1daedecf987779
OpenShift guest <zlqhem@daum.net>
2012-12-10 19:26:49 -0500

    fixed index out of range error. if condition should be true only if it has a form of archive reference

--- read.c
+++ read.c
@@ -3021,7 +3021,7 @@ parse_file_seq (char **stringp, unsigned int size, int stopchar,
          character, so ensure there's some word ending like that before
          considering this an archive group.  */
       if (! (flags & PARSEFS_NOAR)
-          && tp == tmpbuf && tp[0] != '(' && tp[nlen-1] != ')')
+          && tp == tmpbuf && tp[0] != '(' && tp[nlen-1] == ')')
         {
           char *n = strchr (tp, '(');
           if (n)
