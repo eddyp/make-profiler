7edd804a02829c8cf0fd66dd3a7d86f2bb92b65d
Eddy Petrișor <eddy.petrisor@gmail.com>
2013-11-06 02:18:06 +0200

    explain what print_target_update_time_flag is for
    Signed-off-by: Eddy Petrișor <eddy.petrisor@gmail.com>

--- main.c
+++ main.c
@@ -185,6 +185,8 @@ int print_data_base_flag = 0;
 
 int print_graph_flag = 0;
 
+/* Nonzero means print time stamps for start and finish for each of the
+   targets in the makefile. */
 int print_target_update_time_flag = 0;
 
 /* Nonzero means don't remake anything; just return a nonzero status
